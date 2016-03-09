bd6f22aeb7e91bab8134b450f8bc280b1a1b00f7
xibeilang <wdx1004wdx@163.com>
2016-03-09 22:45:47 +0800

    Build wifimanage application using ManagedWifi API
    调用ManagedWifi API库编写了一个wifi管理应用程序，可以用于wince等嵌入式系统的wifi管理。

---
+++ WifiManager/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NativeWifi;
+
+namespace WifiManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public WIFISSID cmccWifiSSID;
+
+    }
+    class WIFISSID
+    {
+        public string SSID = "NONE";
+        public string dot11DefaultAuthAlgorithm = "";
+        public string dot11DefaultCipherAlgorithm = "";
+        public bool networkConnectable = true;
+        public string wlanNotConnectableReason = "";
+        public int wlanSignalQuality = 0;
+        public WlanClient.WlanInterface wlanInterface = null;
+    }
+}
